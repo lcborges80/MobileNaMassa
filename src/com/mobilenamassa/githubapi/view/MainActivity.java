baacca7d18ba449f71425b779ec7f72ae49fbc1a
lcborges80 <lborges@trevisantecnologia.com.br>
2013-12-08 18:34:23 -0200

    change result view on MainActivity

--- src/com/mobilenamassa/githubapi/view/MainActivity.java
+++ src/com/mobilenamassa/githubapi/view/MainActivity.java
@@ -1,13 +1,18 @@
 package com.mobilenamassa.githubapi.view;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,6 +23,8 @@ import com.mobilenamassa.githubapi.model.Commits;
 public class MainActivity extends Activity {
 
 	private SharedPreferences sharedPreferences;
+	private Button verifyCommits;
+	private LinearLayout resultContainer;
 	private TextView numberOfCommitsForPeriod;
 	private AlertDialog downloadMessage;
 
@@ -26,8 +33,10 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main_activity);
 		this.sharedPreferences = getSharedPreferences(SettingsActivity.MY_SHAREDPREFERENCES, MODE_PRIVATE);
+		this.verifyCommits = (Button) findViewById(R.id.verifyCommits);
+		this.resultContainer = (LinearLayout) findViewById(R.id.resultContainer);
 		this.numberOfCommitsForPeriod = (TextView) findViewById(R.id.numberOfCommitsForPeriod);
-		this.downloadMessage = createDialog("Downloading commits...", true);
+		this.downloadMessage = createDialog(getResources().getString(R.string.dialogDownload));
 	}
 
 	@Override
@@ -50,80 +59,68 @@ public class MainActivity extends Activity {
 		if (this.sharedPreferences.getAll().isEmpty()) {
 			Toast.makeText(this, getResources().getString(R.string.noSettingsMessage), Toast.LENGTH_LONG).show();
 		} else {
-			GithubCommitsManager githubCommitsManager = new GithubCommitsManager(this);
-			int period = this.sharedPreferences.getInt(SettingsActivity.PERIOD, 0);
-			String username = this.sharedPreferences.getString(SettingsActivity.USERNAME, "");
-			String password = this.sharedPreferences.getString(SettingsActivity.PASSWORD, "");
-			String repository = this.sharedPreferences.getString(SettingsActivity.REPOSITORY, "");
-			String startDate = githubCommitsManager.getStartDateISO8601Format(period);
-			String url = githubCommitsManager.createUrl(username, repository);
-			
-			try {
-				Commits[] commits = githubCommitsManager.getCommitsForPeriod(url, password, startDate);
-				this.numberOfCommitsForPeriod.setText(String.valueOf(commits.length));
-			} catch (Exception exception) {
-				this.numberOfCommitsForPeriod.setText(exception.toString());
-			}
-			
-			
-			
-//			new AsyncTask<Void, Void, HttpRequest>() {
-//
-//				@Override
-//				protected void onPreExecute() {
-//					super.onPreExecute();
-//					downloadMessage.show();
-//				}
-//
-//				@Override
-//				protected HttpRequest doInBackground(Void... params) {
-//					HttpRequest httpRequest = HttpRequest.get("https://api.github.com/repos/lcborges80/MobileNaMassa/commits", true, "since", "2013-12-01T02:00:00Z", "author", "lcborges80");
-//					httpRequest.basic("mnmworkshop", "mnm_pass2013");
-//					Log.i("MyTAG", httpRequest.header("X-RateLimit-Remaining"));
-//					return httpRequest;
-//				}
-//
-//				@Override
-//				protected void onPostExecute(HttpRequest request) {
-//					downloadMessage.dismiss();
-//					String result = request.body();
-//					if (TextUtils.isEmpty(result)) {
-//						createDialog("Connection or JSON Parse error", false).show();
-//					} else {
-//						Gson gson = new Gson();
-//						MainCommit[] mainCommits = gson.fromJson(result, MainCommit[].class);
-//						StringBuilder bufferMessage = new StringBuilder();
-//						for (MainCommit mainCommit : mainCommits) {
-//							bufferMessage.append("date: ");
-//							bufferMessage.append(mainCommit.getCommit().getCommitter().getDate());
-//							bufferMessage.append('\n');
-//							bufferMessage.append("message: " + mainCommit.getCommit().getMessage());
-//							bufferMessage.append("\n\n");
-//						}
-//						createDialog(bufferMessage.toString(), false).show();
-//					}
-//				}
-//
-//			}.execute();
+
+			new AsyncTask<Void, Void, Void>() {
+
+				@Override
+				protected void onPreExecute() {
+					super.onPreExecute();
+					verifyCommits.setEnabled(false);
+					downloadMessage.show();
+				}
+
+				@Override
+				protected Void doInBackground(Void... params) {
+
+					GithubCommitsManager githubCommitsManager = new GithubCommitsManager(MainActivity.this);
+					int period = sharedPreferences.getInt(SettingsActivity.PERIOD, 0);
+					String username = sharedPreferences.getString(SettingsActivity.USERNAME, "");
+					String password = sharedPreferences.getString(SettingsActivity.PASSWORD, "");
+					String repository = sharedPreferences.getString(SettingsActivity.REPOSITORY, "");
+					String startDate = githubCommitsManager.getStartDateISO8601Format(period);
+					String url = githubCommitsManager.createUrl(username, repository);
+
+					try {
+						final Commits[] commits = githubCommitsManager.getCommitsForPeriod(url, password, startDate);
+						numberOfCommitsForPeriod.post(new Runnable() {
+
+							public void run() {
+								numberOfCommitsForPeriod.setText(commits == null ? "null" : String.valueOf(commits.length));
+							}
+
+						});
+
+					} catch (final Exception exception) {
+						numberOfCommitsForPeriod.post(new Runnable() {
+
+							public void run() {
+								numberOfCommitsForPeriod.setText(exception.toString());
+							}
+
+						});
+
+					}
+					return null;
+				}
+
+				@Override
+				protected void onPostExecute(Void result) {
+					downloadMessage.dismiss();
+					verifyCommits.setEnabled(true);
+					resultContainer.setVisibility(View.VISIBLE);
+				}
+
+			}.execute();
 
 		}
 
 	}
 
-	private AlertDialog createDialog(String message, boolean isDownloadDialog) {
+	private AlertDialog createDialog(String message) {
 		AlertDialog.Builder builder = new AlertDialog.Builder(this);
-		builder.setTitle("Message");
-		AlertDialog alertDialog;
-		if (isDownloadDialog) {
-			View view = getLayoutInflater().inflate(R.layout.data_download, null);
-			((TextView) view.findViewById(R.id.downloadMessage)).setText(message);
-			alertDialog = builder.create();
-			alertDialog.setView(view);
-		} else {
-			builder.setMessage(message);
-			alertDialog = builder.create();
-		}
-		return alertDialog;
+		builder.setTitle(R.string.dialogTitle);
+		builder.setMessage(message);
+		return builder.create();
 	}
 
 }
